bf4d44c7fe404e00f1f5207dba442bfcd13dbd7c
Christian Hofer <christian.hofer@edu.fh-joanneum.at>
2017-04-06 15:49:46 +0200

    Chanced ArrayList bestandteile to String bestandteile

--- WineOS/src/at/fhj/swd/WineOS/Tables/Fertigungsanlage.java
+++ WineOS/src/at/fhj/swd/WineOS/Tables/Fertigungsanlage.java
@@ -1,39 +1,43 @@
 package at.fhj.swd.WineOS.Tables;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.*;
 
 @Entity
-@Table (name = "dbo.Fertigungsanlage")
+@Table(name = "dbo.Fertigungsanlage")
 public class Fertigungsanlage {
 
-	//primary key
-	@Id private int id;
-	
+	// primary key
+	@Id
+	private int id;
+
 	// Beziehung zu Weingut 1:1 Source ist Weingut
-	@OneToOne @JoinColumn(name = "FK_Weingut") private Weingut weingut;
-	
+	@OneToOne
+	@JoinColumn(name = "FK_Weingut")
+	private Weingut weingut;
+
 	private String bezeichnung;
-	private List<String> bestandteile = new ArrayList<String>();
+	private String bestandteile = null;
 	private int volumen;
-	
-	public Fertigungsanlage(){}
-	
+
+	public Fertigungsanlage() {
+	}
+
 	public Fertigungsanlage(int id, String bezeichnung, int volumen, Weingut wg) {
 		setBezeichnung(bezeichnung);
 		setId(id);
 		setVolume(volumen);
 		setWeingut(wg);
 	}
-	
+
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
-		if(id <= 0)
+		if (id <= 0)
 			throw new IllegalArgumentException();
 		this.id = id;
 	}
@@ -43,39 +47,46 @@ public class Fertigungsanlage {
 	}
 
 	public void setBezeichnung(String bezeichnung) {
-		if(bezeichnung == null)
+		if (bezeichnung == null)
 			throw new IllegalArgumentException();
 		this.bezeichnung = bezeichnung;
 	}
 
 	public List<String> getBestandteile() {
-		return bestandteile;
+		List<String> res = Arrays.asList(bestandteile.split(";"));
+		return res;
 	}
 
 	public void addBestandteile(String bestandteil) {
-		if(bestandteil == null)
+		if (bestandteil == null)
 			throw new IllegalArgumentException();
-		bestandteile.add(bestandteil);
+		StringBuilder br = new StringBuilder();
+		if (this.bestandteile == null) {
+			br.append(bestandteil);
+		} else {
+			br.append(this.bestandteile).append(";").append(bestandteil);
+		}
+		this.bestandteile = br.toString();
 	}
 
-	public void setWeingut(Weingut wg){
-		if(wg == null)
+	public void setWeingut(Weingut wg) {
+		if (wg == null)
 			throw new IllegalArgumentException();
 		this.weingut = wg;
 		this.weingut.setAnlage(this);
 	}
-	
+
 	public Object getWeingut() {
 		return weingut;
 	}
-	
-	public void setVolume(int i){
-		if(i <= 0)
+
+	public void setVolume(int i) {
+		if (i <= 0)
 			throw new IllegalArgumentException();
 		this.volumen = i;
 	}
-	
-	public int getVolume(){
+
+	public int getVolume() {
 		return volumen;
 	}
 
@@ -100,12 +111,5 @@ public class Fertigungsanlage {
 			return false;
 		return true;
 	}
-	
-	
 
-
-
-	
-	
-	
 }
